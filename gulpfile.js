f40dd424b7c60c0731a55d3742e4afa7cf5b557f
Antonio Sferruzza <antonio.sferruzza@accenture.com>
2020-12-17 16:40:04 +0100

    add postcss part of build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ const {task, src, dest} = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const postcssCustomProperties = require('postcss-custom-properties');
+
+const browser_support = ['last 2 versions', 'Firefox >= 51', 'iOS >= 8', 'ie 11'];
+
 task('default', () => {
     return src('*.{scss,css}', {cwd: '.'})
         .pipe(sass().on('error', sass.logError))
@@ -9,6 +15,15 @@ task('default', () => {
             sass({
                 outputStyle: 'expanded',
             }).on('error', sass.logError)
+        )
+        .pipe(
+            postcss([
+                postcssCustomProperties(),
+                autoprefixer({
+                    grid: 'autoplace',
+                    overrideBrowserslist: browser_support,
+                }),
+            ])
         )
         .pipe(dest('./'));
 });
